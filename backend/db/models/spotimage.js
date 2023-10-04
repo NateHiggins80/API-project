13a5e3966a5cfc4b006ed984ace6d2aed8ddf0d0
Nate Higgins <higginsn80@gmail.com>
2023-10-04 14:00:43 -0600

    spot routes completed(auth needed)

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -13,8 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       SpotImage.belongsTo(models.Spot, {
         foreignKey: "spotId",
-        onDelete: "CASCADE",
-        hooks: true
+       
       });
     }
   }
