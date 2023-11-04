5f5cdf710f9fe357cb96e30f30cae2f5b371bbed
Nate Higgins <higginsn80@gmail.com>
2023-11-04 11:08:01 -0600

    final route work

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -52,7 +52,7 @@ const validateReview = [
 ];
 const validateQuery = [
   check('page')
-    optional()
+    .optional()
     .isInt({min:1, max:10})
     .withMessage("Page must be greater than or equal to 1"),
   check('size')
